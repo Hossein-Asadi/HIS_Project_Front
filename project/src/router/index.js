fb69a21a15a103403c228b67db8d7f863637f0ab
Hossein-Asadi <hosseinasd0109@gmail.com>
2024-01-27 21:58:46 +0330

    add change of landing page

--- project/src/router/index.js
+++ project/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '@/views/Login.vue'
+import LandingPage from '@/views/LandingPage.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,10 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: Login
+  },{
+    path: '/',
+    name: 'LandingPage',
+    component: LandingPage
   },
 ]
 
