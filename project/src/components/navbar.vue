348772a08391367fc4f1b2b497afc8ca3c085b20
mostafa sohbati <mostafa.sohbati2@gmail.com>
2024-01-30 23:23:08 +0330

    fix: navbar

--- project/src/components/navbar.vue
+++ project/src/components/navbar.vue
@@ -2,7 +2,7 @@
   <div class="navbar-container">
     <div class="navbar-item">
       <a
-        v-if="userRole == 'patient'"
+        v-if="userRole == 'pharmacy'"
         style="height: 20px; width: 80px; margin-left: 40px"
         @click="pharmacy"
         >داروخانه
