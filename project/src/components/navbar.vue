34854f76a936fb7df0253634f47dc5b3b6c0d90c
Hossein-Asadi <hosseinasd0109@gmail.com>
2024-01-30 15:49:13 +0330

    :sparkles: add selection to patient list

--- project/src/components/navbar.vue
+++ project/src/components/navbar.vue
@@ -1,79 +1,105 @@
 <template>
   <div class="navbar-container">
     <div class="navbar-item">
-      <a v-if="userRole == 'doctor'" style="height: 20px; width: 80px; margin-left: 40px;" @click="patientsList">لیست بیماران </a>
-      <a v-if="userRole == 'patient'" style="height: 20px; width: 80px;" @click="doctorList">تقویم پزشکان </a>
-      <a v-if="userRole == 'patient'" style="height: 20px; width: 80px; margin-right: 40px;" @click="onlineReservation">نوبت دهی آنلاین</a>
+      <a
+        v-if="userRole == 'doctor'"
+        style="height: 20px; width: 80px; margin-left: 40px"
+        @click="patientsList"
+        >لیست بیماران
+      </a>
+      <a
+        v-if="userRole == 'patient'"
+        style="height: 20px; width: 80px"
+        @click="doctorList"
+        >تقویم پزشکان
+      </a>
+      <a
+        v-if="userRole == 'patient'"
+        style="height: 20px; width: 80px; margin-right: 40px"
+        @click="onlineReservation"
+        >نوبت دهی آنلاین</a
+      >
     </div>
     <div class="info-item">
-      <a class="user-info" @click="userInfoToggle">
-          مشخصات
-      </a>
-      <div class="user-info-box" :style="{ display: userInfoOpen ? 'block' : 'none' }" >
-        <profile @changeUserInfo="changeUserInfo" :name=userFullName :national-number=userInfo.national_number :gender=userInfo.sex :username=userInfo.username :password=userInfo.password :birthdate=userInfo.birthdate :userInfo=userInfo></profile>
+      <a class="user-info" @click="userInfoToggle"> مشخصات </a>
+      <div
+        class="user-info-box"
+        :style="{ display: userInfoOpen ? 'block' : 'none' }"
+      >
+        <profile
+          @changeUserInfo="changeUserInfo"
+          :name="userFullName"
+          :national-number="userInfo.national_number"
+          :gender="userInfo.sex"
+          :username="userInfo.username"
+          :password="userInfo.password"
+          :birthdate="userInfo.birthdate"
+          :userInfo="userInfo"
+        ></profile>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import profile from '@/components/profile.vue'
-import api from '@/api'
+import profile from "@/components/profile.vue";
+import api from "@/api";
 
 export default {
-  name: 'navbar',
-  components: {profile},
+  name: "navbar",
+  components: { profile },
   data() {
     return {
       userInfoOpen: false,
-      userRole: '',
+      userRole: "",
       userInfo: null,
-    }
+    };
   },
   async created() {
-    this.userRole = JSON.parse(localStorage.getItem('role'))
-    this.changeUserInfo()
+    this.userRole = JSON.parse(localStorage.getItem("role"));
+    this.changeUserInfo();
   },
   computed: {
     userFullName() {
       if (this.userInfo != null)
-        return this.userInfo.first_name + ' ' + this.userInfo.last_name
-      return ''  
-    }
+        return this.userInfo.first_name + " " + this.userInfo.last_name;
+      return "";
+    },
   },
   methods: {
     doctorList() {
-      this.$emit('doctorList')
+      this.$emit("doctorList");
     },
     onlineReservation() {
-      this.$emit('onlineReservation')
+      this.$emit("onlineReservation");
     },
     patientsList() {
-      this.$emit('patientsList')
-    },pharmacy() {
-      this.$emit('pharmacy')
+      this.$emit("patientsList");
+    },
+    pharmacy() {
+      this.$emit("pharmacy");
     },
     userInfoToggle() {
       if (this.userInfoOpen) {
         this.userInfoOpen = false;
-      }
-      else {
+      } else {
         this.userInfoOpen = true;
       }
     },
     async changeUserInfo() {
-      let userId = JSON.parse(localStorage.getItem('userId'))
+      let userId = JSON.parse(localStorage.getItem("userId"));
 
-      await api.getUserInfo(userId)
-      .then(({data}) => {
-        this.userInfo = data
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-    }
-  }
-}
+      await api
+        .getUserInfo(userId)
+        .then(({ data }) => {
+          this.userInfo = data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
 </script>
 
 <style>
@@ -88,29 +114,30 @@ export default {
   border-radius: 5px;
   color: white;
 }
-.navbar-item{
+.navbar-item {
   width: 50%;
   padding-left: 10px;
   padding-top: 15px;
   direction: ltr;
   cursor: pointer;
 }
-.info-item{
+.info-item {
   width: 50%;
   padding-left: 10px;
   padding-top: 15px;
   direction: rtl;
   cursor: pointer;
 }
-.user-info{
+.user-info {
   position: relative;
   height: 20px;
   width: 80px;
   margin-right: 40px;
 }
-.user-info-box{
+.user-info-box {
   position: absolute;
   top: 50px;
   right: 40px;
+  z-index: 100;
 }
 </style>
--- project/src/components/patientsList.vue
+++ project/src/components/patientsList.vue
@@ -1,48 +1,148 @@
 <template>
   <div dir="rtl" class="box-layout">
+    <div style="width: 30%">
+      <div style="margin-bottom: 10px">
+        <p>لطفا روز مورد نظر را انتخاب کنید:</p>
+      </div>
+      <div class="select">
+        <select v-model="selectedOption">
+          <option
+            v-for="(day, index) in days"
+            :value="index"
+            :selected="index === selectedOption"
+          >
+            {{ day }}
+          </option>
+        </select>
+      </div>
+    </div>
     <div class="list-layout">
-    
-      <patientInfo name="محمدرضا قادری" age="2" insurement=false illness="کصخلیت مزمن" visit-time="1403/2/1"/>
-      <patientInfo name="محمدرضا قادری" age="2" insurement=false illness="کصخلیت مزمن" visit-time="1403/2/1"/>
-      <patientInfo name="محمدرضا قادری" age="2" insurement=false illness="کصخلیت مزمن" visit-time="1403/2/1"/>
-      <patientInfo name="محمدرضا قادری" age="2" insurement=false illness="کصخلیت مزمن" visit-time="1403/2/1"/>
-      <patientInfo name="محمدرضا قادری" age="2" insurement=false illness="کصخلیت مزمن" visit-time="1403/2/1"/>
-      <patientInfo name="محمدرضا قادری" age="2" insurement=false illness="کصخلیت مزمن" visit-time="1403/2/1"/>
-      <patientInfo name="محمدرضا قادری" age="2" insurement=false illness="کصخلیت مزمن" visit-time="1403/2/1"/>
-
+      <patientInfo
+        v-for="(patient, index) in filteredPatients"
+        :key="index"
+        :name="patient.name"
+        :visit-time="
+          patient.visitTime.substring(2).split('-').reverse().join('-')
+        "
+        :age="patient.age"
+      />
     </div>
   </div>
 </template>
-  
+
 <script>
-import patientInfo from './patientInfo.vue';
+import patientInfo from "./patientInfo.vue";
 
 export default {
   name: "patientsList",
-  components: {patientInfo},
+  components: { patientInfo },
   data() {
     return {
-      
-    }
+      patients: [
+        { name: "چهارشنبه", age: "4", visitTime: "0-8:15-8:30" },
+        { name: "چهارشنبه", age: "4", visitTime: "2-8:15-8:30" },
+        { name: "چهارشنبه", age: "4", visitTime: "3-8:15-8:30" },
+        { name: "چهارشنبه", age: "4", visitTime: "4-8:15-8:30" },
+        { name: "چهارشنبه", age: "4", visitTime: "0-8:15-8:30" },
+        { name: "چهارشنبه", age: "4", visitTime: "1-8:15-8:30" },
+        { name: "چهارشنبه", age: "4", visitTime: "5-8:15-8:30" },
+        { name: "چهارشنبه", age: "4", visitTime: "6-8:15-8:30" },
+        { name: "چهارشنبه", age: "4", visitTime: "0-8:15-8:30" },
+        { name: "چهارشنبه", age: "4", visitTime: "1-8:15-8:30" },
+        { name: "چهارشنبه", age: "4", visitTime: "6-8:15-8:30" },
+        { name: "چهارشنبه", age: "4", visitTime: "6-8:15-8:30" },
+        { name: "چهارشنبه", age: "4", visitTime: "5-8:15-8:30" },
+      ],
+      selectedOption: 0,
+    };
   },
-}
+  computed: {
+    filteredPatients() {
+      return this.patients.filter(
+        (patient) => patient.visitTime.charAt(0) == this.selectedOption
+      );
+    },
+    days() {
+      return [
+        "شنبه",
+        "یکشنبه",
+        "دوشنبه",
+        "سه شنبه",
+        "چهارشنبه",
+        "پنچشنبه",
+        "جمعه",
+      ];
+    },
+  },
+};
 </script>
 
 <style scoped>
-.box-layout{
+.box-layout {
   display: flex;
   justify-content: center;
   align-items: center;
   width: 100%;
   height: 90vh;
 }
-.list-layout{
+.list-layout {
   display: flex;
   flex-direction: column;
   width: 60%;
   height: 80vh;
   background-color: #99bbf5;
+  color: white;
   overflow-y: auto;
   border-radius: 10px;
+}
+
+select {
+  appearance: none;
+  border: 0;
+  outline: 0;
+  background: #99bbf5;
+  color: inherit;
+  box-shadow: none;
+}
+select::-ms-expand {
+  display: none;
+}
+/* Custom Select wrapper */
+.select {
+  position: relative;
+  display: flex;
+  width: min(20rem, 90vw);
+  background: var(--background-select);
+  border-radius: 0.25rem;
+  overflow: hidden;
+
+  select {
+    flex: 1;
+    padding: 1em;
+    cursor: pointer;
+  }
+
+  &::after {
+    content: "\25BC";
+    position: absolute;
+    left: 1rem;
+    top: 1rem;
+    transition: 0.25s all ease;
+    pointer-events: none;
+    direction: ltr;
+  }
+
+  &:hover::after {
+    color: gray;
+    animation: bounce 0.5s infinite;
+  }
+}
+@keyframes bounce {
+  25% {
+    transform: translatey(5px);
+  }
+  75% {
+    transform: translatey(-5px);
+  }
 }
 </style>
